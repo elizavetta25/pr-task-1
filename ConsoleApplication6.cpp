2fcc1eb0e7a5e410ce2f1d807db8d6555759c5cd
elizavetta25 <124656076+elizavetta25@users.noreply.github.com>
2023-09-08 12:04:50 +0300

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	// Задание №1
+	setlocale(LC_ALL, "Rus");
+
+	try
+	{
+		double t, l;
+
+		cout << "Введите переменную t" << endl;
+		cin >> t;
+
+		cout << "Введите переменную l" << endl;
+		cin >> l;
+
+		cout << "R = " << 3 * pow(t, 2) + 3 * pow(l, 5) + 4.9 << endl;
+	}
+	catch (const exception&)
+	{
+		cout << "Ошибка входных данных" << endl;
+	}
+
+
+
+	system("pause");
+}
+
